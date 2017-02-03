230b23f884bae1067f18b48c3461921b9a36a380
maurilio-ufpb <mauriliobage@yahoo.com.br>
2017-02-03 12:41:22 -0300

    aula1 python

---
+++ DespesaRestaurante.py
@@ -0,0 +1,14 @@
+
+
+
+#custoRefeicao= float(input("Informe o valor a ser pago: "))
+
+custoRefeicao= float(input("Informe o valor: "))
+
+#custoRefeicao= 44.50
+imposto = custoRefeicao * 0.0675
+custoTotal= (custoRefeicao + imposto)
+gorgeta= custoTotal * 0.15
+
+print("O Custo total é Igual: ", custoTotal)
+print("O valor da gorgeta é: ", gorgeta)
