258ca2e43ee9eef111a5275d8b1deb21173b29f9
maurilio-ufpb <mauriliobage@yahoo.com.br>
2017-02-19 23:26:43 -0300

    Exercicio 2

---
+++ Multiplicacao_dois_numeros.py
@@ -0,0 +1,8 @@
+# 2. Construa um algoritmo que compute o resultado da multiplicação entre dois
+#números.
+
+num1= int(input("Informe o número: "))
+num2= int(input("Informe o número: "))
+resultado= num1 * num2
+print("O resultado da multiplicação é: ", resultado)
+
