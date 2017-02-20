739f9eb7e525360dc5bfd6cdb28e5ffbb814c6dc
maurilio-ufpb <mauriliobage@yahoo.com.br>
2017-02-19 23:35:02 -0300

    Exercicio 13

---
+++ Sao_multiplos_nao_multiplos.py
@@ -0,0 +1,13 @@
+
+
+# 13. Elaborar um algoritmo que lê 2 valores a e b e os escreve com a mensagem:
+# "São múltiplos" ou "Não são múltiplos".
+
+num1= int(input("Informe o primeiro número: "))
+num2= int(input("Informe o segundo número: "))
+if num1>num2 and num1%num2== 0:
+    print("São Múltiplos!!")
+else:
+    print("Não sao Múltiplos!!")
+
+                
